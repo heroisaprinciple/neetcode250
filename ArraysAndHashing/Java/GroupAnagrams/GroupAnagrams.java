6bf154b320c18ff7dcae3ba81715bc242d9c2a19
Arina <valcat552@gmail.com>
2025-07-05 22:16:59 +0100

    add q6 in java

---
+++ ArraysAndHashing/Java/GroupAnagrams/GroupAnagrams.java
@@ -0,0 +1,39 @@
+/*
+ * Time complexity: O(n * m log n), where:
+ *  - n = length of the strs array
+ *  - m = length of the LONGEST word
+ * Space complexity: O(n * m)
+ *
+ * The logic:
+ * Each sorted word is a key in the hashmap, and the original words
+ * (not sorted) that match that sorted key are added to a list -> the value.
+ */
+
+package Java.GroupAnagrams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+class GroupAnagram {
+  public static List<List<String>> groupAnagrams(String[] strs) {
+    HashMap<String, List<String>> anagrams = new HashMap<>();
+    for (String word : strs) {
+      char[] charArr = word.toCharArray();
+      Arrays.sort(charArr); // O(n * m log n)
+      String sorted = new String(charArr);
+
+      if (!anagrams.containsKey(sorted)) {
+        anagrams.put(sorted, new ArrayList<>());
+      }
+      anagrams.get(sorted).add(word);
+    }
+    return new ArrayList<>(anagrams.values());
+  }
+
+  public static void main(String[] args) {
+    String[] strs = { "act", "pots", "tops", "cat", "stop", "hat" };
+    groupAnagrams(strs);
+  }
+}
---
+++ ArraysAndHashing/Java/GroupAnagrams/GroupAnagramsOptimized.java
@@ -0,0 +1,41 @@
+/*
+ * Time complexity: O(n * m), where:
+ *  - n = length of the strs array
+ *  - m = length of the LONGEST word
+ * Space complexity: O(n * m)
+ *
+ * The logic:
+ * Each word is converted into a freq array of 26 integers
+ * (how many times the letter appears)
+ * The freq array is used as a hashmap key and words with the same n of freq
+ * will become values of a hashmap
+ */
+
+package Java.GroupAnagrams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class GroupAnagramsOptimized {
+  public static List<List<String>> groupAnagrams(String[] strs) {
+    HashMap<String, List<String>> anagrams = new HashMap<>();
+    for (String word : strs) {
+      int[] count = new int[26];
+      for (char c : word.toCharArray()) {
+        count[c - 'a']++;
+      }
+      String key = Arrays.toString(count); // the freq array converted into a str
+      System.out.println(key); // "[1, 0, 1, 0....]"
+      anagrams.putIfAbsent(key, new ArrayList<>());
+      anagrams.get(key).add(word);
+    }
+    return new ArrayList<>(anagrams.values());
+  }
+
+  public static void main(String[] args) {
+    String[] strs = { "act", "pots", "tops", "cat", "stop", "hat" };
+    groupAnagrams(strs);
+  }
+}
